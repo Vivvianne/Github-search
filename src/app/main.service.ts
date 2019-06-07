17ba42ca9fedb0bf9b9d8ecf5510cb10455e1cab
Vivvianne Kimani <ndiahav@gmail.com>
2019-06-07 16:00:59 +0300

    add syntax in repository.html

--- src/app/main.service.ts
+++ src/app/main.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http'
+import { environment } from "../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MainService {
+  
+  constructor(private http: HttpClient) { }}
 
-  constructor() { }
-}
+  // getProfile(name:string){
+  //   return this.http.get(environment.apiUrl+name+"?access_token="+environment.gitaccesstoken);
+  // }
+  // getRepos(url){
+  //   return this.http.get(url);
+  // }
+  // getRepo(name){
+  //   return this.http.get("https://api.github.com/repos/"+name);
+  // }
+  
+  
+  
+
