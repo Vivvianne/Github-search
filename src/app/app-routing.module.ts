c1e658f774118fa7ca1cee01a9f567d9efbcafb9
Vivvianne Kimani <ndiahav@gmail.com>
2019-06-07 17:25:20 +0300

    import user and repository componets in the app-routing-module.ts, added the path and componets for both user and repository componets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserComponent} from './user/user.component';
+import { RepositoryComponent} from './repository/repository.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: UserComponent
+  },
+  {
+    path: 'repo,/:name',
+    component: RepositoryComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
