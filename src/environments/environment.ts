5516d63efc236551d45fa9168adb2a0ff9bd683e
Vivvianne Kimani <ndiahav@gmail.com>
2019-06-08 12:23:54 +0300

    commit all changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  gitaccesstoken:"6f76836cc5dab677562f5dc22673565fb2714284",
+  
+  apiUrl:"https://api.github.com/users/"
 };
 
 /*
